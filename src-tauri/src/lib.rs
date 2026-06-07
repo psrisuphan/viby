5c7f49716ec95a316983c9686050da4ee1543405
Bukutsu <koonbook456@gmail.com>
2026-06-07 19:14:15 +0700

    fix: run single-instance callback window operations on the main thread to prevent deadlock

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -102,10 +102,13 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
-            if let Some(window) = app.get_webview_window("main") {
-                let _ = window.show();
-                let _ = window.set_focus();
-            }
+            let app_clone = app.clone();
+            let _ = app.run_on_main_thread(move || {
+                if let Some(window) = app_clone.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            });
         }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
